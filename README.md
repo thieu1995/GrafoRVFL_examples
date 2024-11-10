ca90e302ca2f24df086969690be857c74fccbc62
anh9895 <honganh090895@gmail.com>
2024-11-11 00:33:44 +0700

    Update

---
+++ README.md
@@ -0,0 +1,14 @@
+# GrafoRVFL Examples
+
+### Setup environment
+
+```bash
+pip install -r requirements.txt
+```
+
+### Run the scripts
+
+```bash
+python 01_iris.py
+....
+```
---
+++ main_run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+python 02_breast_cancer.py &&
+python 03_digits.py &&
+python 04_wine.py &&
+python 05_phoneme.py &&
+python 06_waveform.py &&
+python 07_magic_telescope.py &&
+python 08_diabetes.py &&
+python 09_boston.py &&
+python 10_california.py
