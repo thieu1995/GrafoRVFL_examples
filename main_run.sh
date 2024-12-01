2c056ffe7cb490395d230fa74ccc414fc943716a
Thieu Nguyen <nguyenthieu2102@gmail.com>
2024-12-02 01:10:49 +0700

    Update main script sh

--- main_run.sh
+++ main_run.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-python 02_breast_cancer.py &&
-python 03_digits.py &&
-python 04_wine.py &&
-python 05_phoneme.py &&
-python 06_waveform.py &&
-python 07_magic_telescope.py &&
-python 08_diabetes.py &&
-python 09_boston.py &&
-python 10_california.py
+python 01_breast_cancer.py &&
+python 02_waveform.py &&
+python 03_magic_telescope.py &&
+python 04_diabetes.py &&
+python 05_boston.py &&
+python 06_california.py
